47a4d131fd6aa6fce2590b99c9bf820fe7aa9d15
Chinedu Igwe <cigwe@Chinedus-MacBook-Pro.local>
2016-12-23 11:41:29 -0500

    Set up tournament database

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -7,11 +7,19 @@
 -- these lines here.
 
 DROP DATABASE IF EXISTS Tournament;
-
 CREATE DATABASE Tournament;
+\c tournament;
 
-CREATE TABLE Players(Id Serial, Name Text)
-CREATE TABLE PlayerStandings(Id, PlayerId, 
+CREATE TABLE Players(Id Serial PRIMARY KEY, 
+                     Name VARCHAR(100));
 
-CREATE TABLE Matches
+CREATE TABLE PlayerStandings(Id Serial PRIMARY KEY,
+				PlayerId INTEGER REFERENCES Players(Id) NOT NULL UNIQUE, 
+                                Score INTEGER NOT NULL
+                             ); 
+
+CREATE TABLE Matches(Id Serial PRIMARY KEY, 
+                        WinPlayerId INTEGER REFERENCES Players(Id) NOT NULL, 
+                        LosePlayerId INTEGER REFERENCES Players(Id) NOT NULL,
+                        UNIQUE (WinPlayerId, LosePlayerId));
 
